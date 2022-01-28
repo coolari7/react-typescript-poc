0662a9a2ff4e7fd1a40bd1b85c0a2e4e536e05e8
Arijit Chowdhury <coolari7@gmail.com>
2022-01-28 11:22:04 +0530

    fix: exported all packages

---
+++ packages/ui/src/Animations/index.ts
@@ -0,0 +1,3 @@
+export * from "./Animate";
+export * from "./Fade";
+export * from "./Scale";
---
+++ packages/ui/src/Menu/index.ts
@@ -0,0 +1,3 @@
+export * from "./Menu";
+export * from "./MenuControl";
+export * from "./MenuOptions";
